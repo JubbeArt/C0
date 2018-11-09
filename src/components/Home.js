5f4d8930a1dd01955f5e6cf0754d37deba39e5a5
Jesper Wrang <jeswr740@student.liu.se>
2018-11-09 19:47:01 +0100

    Added globe and footprint

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export default props => (
-  <div>Home</div>
+  <>
+    <div className='globe'>
+      <img className='footprint' src='/assets/footprint.png' />
+    </div>
+    <h3>Dagliga genomsnitt...</h3>
+    <h3>Jämnfört med världen...</h3>
+  </>
 )
