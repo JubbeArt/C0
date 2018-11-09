fc0aac9ec7e740d9be79fc346696523c2b606612
Jesper Wrang <jeswr740@student.liu.se>
2018-11-09 17:32:49 +0100

    Added basic routing logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './components/Home'
+import Transport from './components/Transport'
 
 ReactDOM.render(
-  <div>Hello world!</div>,
+  <Router>
+      {/* <Link to='/'>Home</Link> */}
+    <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route path='/transport'></Route>       
+    </Switch>
+  </Router>,
   document.getElementById('root')
 )
