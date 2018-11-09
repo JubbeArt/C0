7b681f71f8bc545e2d48052d1acda49e711d9ed1
Jesper Wrang <jeswr740@student.liu.se>
2018-11-09 17:50:34 +0100

    Added nav-bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <div>
+    <div>El</div>
+    <div>Inköp</div>
+    <div>Food</div>
+    <div>Transport</div>
+  </div>
+)
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import Home from './components/Home'
 import Transport from './components/Transport'
+import NavBar from './components/NavBar';
 
 ReactDOM.render(
   <Router>
+    <>
       {/* <Link to='/'>Home</Link> */}
     <Switch>
       <Route exact path='/' component={Home}></Route>
-      <Route path='/transport'></Route>       
+      <Route path='/transport'></Route>    
     </Switch>
+    <NavBar />
+    </>
   </Router>,
   document.getElementById('root')
 )
