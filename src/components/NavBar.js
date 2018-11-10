012c4969b3b2f867bfa0437432bdc37ba9f628f1
Jesper Wrang <jeswr740@student.liu.se>
2018-11-10 04:09:06 +0100

    Finished nav-bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom'
 export default props => (
   <div className='nav-bar'>
     <Link className='nav-icon' to='/electricity'>
-      <img src='/assets/batteri.png' style={{ width: '50%' }} />
+      <img src='/assets/batteri.png' style={{ width: '55%', marginTop: '5%' }} />
     </Link>
     <Link className='nav-icon' to='/food'>
       <img src='/assets/gaffel-kniv.png' style={{ width: '45%' }} />
+    </Link>
+    <Link className='home-button' to='/'>
+      <img src='/assets/c0-logo-green-bg-circle.png' style={{ width: '90%' }} />
     </Link>
     <Link className='nav-icon' to='/transport'>
       <img src='/assets/cykel.png' style={{ width: '65%' }} />
@@ -15,8 +18,5 @@ export default props => (
     <Link className='nav-icon' to='/info'>
       <img src='/assets/info.png' style={{ width: '40%' }} />
     </Link>
-    {/* <Link to='/'>
-      <img className='home-button' />
-    </Link> */}
   </div>
 )
