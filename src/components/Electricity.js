3eeadbf4aa141d56bff2764eeefad09bf9a53ee6
Jesper Wrang <jeswr740@student.liu.se>
2018-11-10 06:32:46 +0100

    Started on el

--- src/components/Electricity.js
+++ src/components/Electricity.js
@@ -4,7 +4,13 @@ export default class Electricity extends React.Component {
   render () {
     return (
       <div className='content'>
-        <h1>Electricity</h1>
+        <div className='title'>
+          <img src='/assets/batteri.png' />
+          <span className='big-text'>Energiåtgång</span>
+        </div>
+        <div className='small-text'>Ditt konto är kopplat till Google Home och är synkat</div>
+        <hr />
+        <div className='small-text'>De senaste 5 dagarna</div>
       </div>
     )
   }
--- src/components/Transport.js
+++ src/components/Transport.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from '../state'
-import { addMeal } from '../helpers/algorithms'
 
 const transportType = {
   cycle: 'Cykel',
